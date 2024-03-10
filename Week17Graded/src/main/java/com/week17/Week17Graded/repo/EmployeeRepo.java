afa91539ced03444174d5bcceda3ef9df3c050b7
[Essvaran] <[essvaransekar2410@gmail.com]>
2024-03-10 23:34:34 +0530

    deleting the project

--- Week17Graded/src/main/java/com/week17/Week17Graded/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.week17.Week17Graded.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.week17.Week17Graded.model.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer>{
-
-}
